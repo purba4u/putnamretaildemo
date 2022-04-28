8366f82f596605ba55908b43ce292af9ac8a5cda
Purba Dutta <purba_dutta@putnam.com>
2022-04-28 01:37:34 -0400

    retail test case

---
+++ Test Suites/DemoTest.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>907fb4f1-e695-4a65-8423-205db737c775</testSuiteGuid>
+   <testCaseLink>
+      <guid>5e6e50ee-eeab-450b-bf8a-1a41c7e1702f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/01_Verify_Individual_Site_Fund_Screener_Response_Code</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
